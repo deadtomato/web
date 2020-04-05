8d581c8e475701c00b1d4a68acb88297272d5e6f
deadtomato <ddkid@mail.ru>
2020-04-06 02:06:12 +0300

    Добавялет меню, ссылки на проекты и статью Git

---
+++ Git/js/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var template = document.querySelector('#zoom').content.querySelector('.zoom');
+var article = document.querySelector('.article');
+var imgs = article.querySelectorAll('.article__img');
+var zoomImg = template.cloneNode(true);
+var closeBtn = zoomImg.querySelector('.close');
+var element = zoomImg.querySelector('.zoom__img');
+
+var removeZoomImg = function () {
+  var element = article.querySelector('.zoom');
+  if (element) {
+    article.removeChild(element);
+  }
+  
+}
+
+var onImgClick = function (evt) {
+  removeZoomImg();
+  element.src = evt.target.src;
+  element.alt = evt.target.alt;
+  article.appendChild(zoomImg);
+  closeBtn.addEventListener('click', onCloseBtnClick);
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+var onCloseBtnClick = function (evt) {
+  evt.preventDefault();
+  removeZoomImg();
+  closeBtn.removeEventListener('click', onCloseBtnClick);
+  document.removeEventListener('keydown', onDocumentKeydown);
+} 
+
+var onDocumentKeydown = function (evt) {
+  if (evt.key === 'Escape') {
+    removeZoomImg();
+  }
+}
+
+imgs.forEach(function (img) {
+  img.addEventListener('click', onImgClick);
+});
